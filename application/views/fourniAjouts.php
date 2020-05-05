9fe3c357a6c9d4a3ecc57bd13fa3290e087f4329
Augustin <56958607+Ashkoort@users.noreply.github.com>
2020-05-05 17:15:52 +0200

    MCD/MLD/MPD fichier travail d'auj

--- application/views/fourniAjouts.php
+++ application/views/fourniAjouts.php
@@ -2,49 +2,27 @@
 <div class="row">
 
 <!----------------------------------------- Formulaire ajouts--------------------------------------------->
+<?php echo form_open_multipart("fournisseurs/fourniAjouts", "class='container col-8'"); ?>
 <div class=" container col-8">
     <div class="col-12">
             <fieldset class="row">
                 <legend>Ajouts</legend> 
-                <form action="" method="POST">                    
-                 <div  class="form-group text-center offset-1.5 w-100">
-                <label for="reference">Nom :</label>
-                <input type="text" id="nom" name="nom" class="form-control fontAwesome" placeholder="Nom..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <label for="telephone">Téléphone :</label>
-                <input type="text" id="telephone" name="telephone" class="form-control fontAwesome" placeholder="Téléphone..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <label for="mail">E-mail :</label>
-                <input type="text" id="mail" name="mail" class="form-control fontAwesome" placeholder="Mail..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <label for="siret">Siret :</label>
-                <input type="number" id="siret" name="siret" class="form-control fontAwesome" placeholder="Siret..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
+                <div  class="form-group text-center offset-3 w-50">
                 <label for="adresse">Adresse :</label>
-                <input type="text" id="adresse" name="adresse" class="form-control fontAwesome" placeholder="Adresse..." >
+                <input type="text" id="adresse" name="adresse" class="form-control fontAwesome" placeholder="Adresse..." value="<?php echo set_value('adresse'); ?>">
+                <?php echo form_error('adresse'); ?>
+                </div>
+                <div  class="form-group text-center offset-3 w-50">
+                <label for="ref">Références :</label>
+                <input type="text" id="ref" name="ref" class="form-control fontAwesome" placeholder="références..." value="<?php echo set_value('ref'); ?>">
+                <?php echo form_error('ref'); ?>
                 </div>
                 <div  class="form-group text-center offset-1.5 w-100">
-                <label for="ville">Ville :</label>
-                <input type="text" id="ville" name="ville" class="form-control fontAwesome" placeholder="ville..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <label for="persJoin">Personne joignable :</label>
-                <input type="text" id="persJoin" name="persJoin" class="form-control fontAwesome" placeholder="Personne Joignable..." >
-                </div>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <label for="prix">Prix :</label>
-                <input type="text" id="prix" name="prix" class="form-control fontAwesome" placeholder="prix..." >
-                </div>
-                </form>
-                <div  class="form-group text-center offset-1.5 w-100">
-                <input type="submit" id="ok" name="ok" value="ajouter">
+                <input class="btn btn-dark" type="submit" id="ok" name="ok" value="ajouter">
                 </div>
             </div>
         </fieldset>
+        <?php echo form_close() ?>
        <br>
     </div>
 </div>
--- application/views/fourni_list.php
+++ application/views/fourni_list.php
@@ -1,81 +1,33 @@
 <div class="table-responsive">
 <table class="table table-striped table-hover table-bordered">
+
+    <h1 class="text-dark">Pour ajouter un nouveau produits</h1>
+    <a href="<?= site_url('fournisseurs/fourniAjouts/');?>"><button class="btn btn-dark offset-2"><i class="fas fa-pencil-alt"></i> Ajouts</button></a>
     <thead>
     <tr>
         <th> </th>
         <th>id</th>
-        <th>Nom</th>
-        <th>Téléphone</th>
-        <th>Mail</th>
-        <th>Siret</th>
         <th>Adresse</th>
-        <th>Ville</th>
-        <th>Personne joignable</th>
-        <th>Prix</th>
+        <th>Référence</th>
+
     </tr>
     </thead>
+    <h1 class="text-dark"> Consultez le tableau des fournisseurs</h1>
     <tbody>
+    <?php
+        foreach($fournisseurs as $fourni)
+        {
+            ?>
+        <tr class="table-active">
+        <td><a href="<?= site_url('fournisseurs/fourniModif/'.$fourni->FOU_ID);?>"><button class="btn btn-dark"><i class="fas fa-pencil-alt"></i> Modification</button></a><a href="<?= site_url('fournisseurs/fourniDel/'.$fourni->FOU_ID);?>"><button class="btn btn-dark"><i class="fas fa-pencil-alt"></i> supprimer</button></a></td>
+                    <td><?=$fourni->FOU_ID?></td>
+                    <td><?=$fourni->FOU_ADRESSE?></td>
+                    <td><?=$fourni->FOU_REF?></td>
 
-        <tr class="table-active">
-            <td><a href="<?php echo site_url("fournisseurs/fourniAjouts");?>" class="btn btn-dark" target="_blank"> Ajouts</a><a href="<?php echo site_url("fournisseurs/fourniModif");?>" class="btn btn-dark" target="_blank"> modif</a><a href="/" class="btn btn-dark" target="_blank"> supp</a></td>
-            <td>5</td>
-            <td>Exemple</td>
-            <td>0632254800</td>
-            <td>exemple@gmail.com</td>
-            <td>1254789654</td>
-            <td>85 rue des exemples</td>
-            <td>Paris</td>
-            <td>M.exemple</td>
-            <td>120</td>
         </tr>
-        <tr class="table-active">
-            <td><a href="<?php echo site_url("fournisseurs/fourniAjouts");?>" class="btn btn-dark" target="_blank"> Ajouts</a><a href="<?php echo site_url("fournisseurs/fourniModif");?>" class="btn btn-dark" target="_blank"> modif</a><a href="/" class="btn btn-dark" target="_blank"> supp</a></td>
-            <td>5</td>
-            <td>Exemple</td>
-            <td>0632254800</td>
-            <td>exemple@gmail.com</td>
-            <td>1254789654</td>
-            <td>85 rue des exemples</td>
-            <td>Paris</td>
-            <td>M.exemple</td>
-            <td>120</td>
-        </tr>
-        <tr class="table-active">
-            <td><a href="<?php echo site_url("fournisseurs/fourniAjouts");?>" class="btn btn-dark" target="_blank"> Ajouts</a><a href="<?php echo site_url("fournisseurs/fourniModif");?>" class="btn btn-dark" target="_blank"> modif</a><a href="/" class="btn btn-dark" target="_blank"> supp</a></td>
-            <td>5</td>
-            <td>Exemple</td>
-            <td>0632254800</td>
-            <td>exemple@gmail.com</td>
-            <td>1254789654</td>
-            <td>85 rue des exemples</td>
-            <td>Paris</td>
-            <td>M.exemple</td>
-            <td>120</td>
-        </tr>
-        <tr class="table-active">
-            <td><a href="<?php echo site_url("fournisseurs/fourniAjouts");?>" class="btn btn-dark" target="_blank"> Ajouts</a><a href="<?php echo site_url("fournisseurs/fourniModif");?>" class="btn btn-dark" target="_blank"> modif</a><a href="/" class="btn btn-dark" target="_blank"> supp</a></td>
-            <td>5</td>
-            <td>Exemple</td>
-            <td>0632254800</td>
-            <td>exemple@gmail.com</td>
-            <td>1254789654</td>
-            <td>85 rue des exemples</td>
-            <td>Paris</td>
-            <td>M.exemple</td>
-            <td>120</td>
-        </tr>
-        <tr class="table-active">
-            <td><a href="<?php echo site_url("fournisseurs/fourniAjouts");?>" class="btn btn-dark" target="_blank"> Ajouts</a><a href="<?php echo site_url("fournisseurs/fourniModif");?>" class="btn btn-dark" target="_blank"> modif</a><a href="/" class="btn btn-dark" target="_blank"> supp</a></td>
-            <td>5</td>
-            <td>Exemple</td>
-            <td>0632254800</td>
-            <td>exemple@gmail.com</td>
-            <td>1254789654</td>
-            <td>85 rue des exemples</td>
-            <td>Paris</td>
-            <td>M.exemple</td>
-            <td>120</td>
-        </tr>
+        <?php
+        }
+?>
 </tbody>
 </table>
 </div>
