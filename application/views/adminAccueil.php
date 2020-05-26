81445bffcd7949d726ae18ce5af04803788b2b27
Augustin <56958607+Ashkoort@users.noreply.github.com>
2020-05-26 16:48:39 +0200

    taff du 26/05/2020

--- application/views/adminAccueil.php
+++ application/views/adminAccueil.php
@@ -1,42 +1,46 @@
-<div class="container-fluid"> 
-            <div class="row"> 
-                <div class="col-12 col-sm-6 col-md-6 col-lg-8 col-xl-12">
+<div class="admin container-fluid row" id="admin">
+            <div class="row col-8"> 
+                <div class="col-12 col-sm-6 col-md-6 col-lg-8 col-xl-12 offset-1">
                     <h1 class="text-dark">Partie Admin</h1>
                     <section>
                         <article>
-                            <h4 classe="success bg-dark">Les Fournisseurs</h4>
+                            <h4 >Les Fournisseurs</h4>
                             <p>Ici vous trouverez tous se qu'il y a à savoir sur les fournisseurs en cliquant sur le boutton ci-dessous</p>
-                            <a href="<?php echo site_url("fournisseurs/fourni_list");?>" class="btn btn-dark offset-3 col-auto" target="_blank"> Fournisseurs</a>
+                            <a href="<?php echo site_url("fournisseurs/fourni_list");?>" class="btn btnpers offset-2 col-auto ml-5" > Fournisseurs</a>
                         </article>
                     </section>
                     <section>
                         <article>
-                            <h4 classe="success bg-dark">Les produits</h4>
+                            <h4 >Les produits</h4>
                             <p>Ici vous trouverez tous se qu'il y a à savoir sur les produits en cliquant sur le boutton ci-dessous</p>
-                            <a href="<?php echo site_url("produits/list");?>" class="btn btn-dark offset-3 col-auto"> Produits</a>
+                            <a href="<?php echo site_url("produits/list");?>" class="btn btnpers offset-2 col-auto ml-5"> Produits</a>
                         </article>
                     </section>
                     <section>
                         <article>
-                            <h4 classe="success bg-dark">Les catégories</h4>
+                            <h4 >Les catégories</h4>
                             <p>Ici vous trouverez tous se qu'il y a à savoir sur les catégories en cliquant sur le boutton ci-dessous</p>
-                            <a href="<?php echo site_url("categories/cat_list");?>" class="btn btn-dark offset-3 col-auto"> Categories</a>
+                            <a href="<?php echo site_url("categories/cat_list");?>" class="btn btnpers offset-2 col-auto ml-5"> Categories</a>
                         </article>
                     </section>
                     <section>
                         <article>
-                            <h4 classe="success bg-dark">Les Clients</h4>
+                            <h4 >Les Clients</h4>
                             <p>Ici vous trouverez tous se qu'il y a à savoir sur les clients en cliquant sur le boutton ci-dessous</p>
-                            <a href="<?php echo site_url("clients/cli_list");?>" class="btn btn-dark offset-3 col-auto" target="_blank"> Clients</a>
+                            <a href="<?php echo site_url("clients/cli_list");?>" class="btn btnpers offset-2 col-auto ml-5" > Clients</a>
                         </article>
                     </section>
                     <section class="mb-5">
                         <article>
-                            <h4 classe="success bg-dark ">Le Personnel </h4>
+                            <h4 >Le Personnel </h4>
                             <p>Ici vous trouverez tous se qu'il y a à savoir sur le personnel en cliquant sur le boutton ci-dessous</p>
-                            <a href="<?php echo site_url("personnels/pers_list");?>" class="btn btn-dark offset-3 col-auto" target="_blank"> Personnel</a>
+                            <a href="<?php echo site_url("personnels/pers_list");?>" class="btn btnpers offset-2 col-auto ml-5" > Personnel</a>
                         </article>
                     </section>
                 </div>
             </div>
+            <div class="col-4">
+                <img src="<?php echo base_url('assets/img/personnel/teste.png') ;?>" alt="lorem" class="img-personnel m-0 p-0 img-fluid"> 
+            </div>        
+
 </div>
